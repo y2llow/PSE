be644f4e0e2572125feeaf98057b5db652338fa5
Ayyildiz <a@gmail.com>
2025-04-10 18:17:03 +0200

    added possible changes in comments to SimPrinter.cpp

--- src/simulatie/SimPrinter.cpp
+++ src/simulatie/SimPrinter.cpp
@@ -16,3 +16,36 @@ void SimPrinter::printStatus(Voertuig const* voertuig, double status) {
         << "-> baan: " << voertuig->getBaan() << "\n"
         << "-> positie: " << voertuig->getPositie() << "\n"<<endl;
 }
+/*
+ * //
+// Created by s0246031@ad.ua.ac.be on 3/17/25.
+//
+
+#include "SimPrinter.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+void SimPrinter::printStatus(Voertuig const* voertuig, double status) {
+    cout << "Tijd: " << status << endl;
+
+    // Bepaal het type voertuig als string
+    string typeStr;
+    switch(voertuig->getType()) {
+        case VoertuigType::AUTO: typeStr = "Auto"; break;
+        case VoertuigType::BUS: typeStr = "Bus"; break;
+        case VoertuigType::BRANDWEERWAGEN: typeStr = "Brandweerwagen"; break;
+        case VoertuigType::ZIEKENWAGEN: typeStr = "Ziekenwagen"; break;
+        case VoertuigType::POLITIECOMBI: typeStr = "Politiecombi"; break;
+        default: typeStr = "Onbekend"; break;
+    }
+
+    cout << "Voertuig " << voertuig->getId() << " (" << typeStr << ")\n"
+         << "-> baan: " << voertuig->getBaan()->getNaam() << "\n"  // Gebruik getNaam() voor de baan
+         << "-> positie: " << voertuig->getPositie() << "\n"
+         << "-> snelheid: " << voertuig->getSnelheid() << "\n"
+         << "-> lengte: " << voertuig->getLength() << "\n"<<endl;
+}
+ */
