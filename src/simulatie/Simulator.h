78117c610ac1f1407e06e4a71751e9fc4f8a86cd
Abdellah <s0246031@ad.ua.ac.be>
2025-04-30 00:43:02 +0200

    cleaned simulator.h

--- src/simulatie/Simulator.h
+++ src/simulatie/Simulator.h
@@ -11,7 +11,6 @@
 #include "../../src/elementen/Verkeerslicht.h"
 #include "../../src/elementen/Bushalte.h"
 #include "../../src/elementen/Voertuiggenerator.h"
-#include "../../src/simulatie/SimPrinter.h"
 
 using namespace std;
 
@@ -20,18 +19,21 @@ class Simulator {
     vector<Baan*> banen;
 
     string graphical_impression;
-    bool isConsistent = true;
 
 public:
+    Simulator() = default;
+    bool is_consistent = true;
+
+
     void addBaan(Baan* b);
     [[nodiscard]] const vector<Baan*> getBanen() const;
 
-    void setConsistency(const bool c) { isConsistent = c; }
+    void setConsistency(const bool c) { is_consistent = c; }
     void geldigeTypen(const string& type);
 
     void simulationRun();
     void makeGraphicalImpression();
-    void generateGraphicsFile();
+    void generateGraphicsFile() const;
 
     void print();
     void printStatus(Voertuig const* voertuig) const;
@@ -40,84 +42,18 @@ public:
 
 
 
-    //2.2
-    //de simulatie grafisch gegeven (als string)
-    // SimPrinter simPrinter;
-    SimPrinter simPr;
-
-public:
-//    simulation(SimPrinter*): simPrinter(SimPrinter()) {
-//    };
-    bool is_consistent = true;
-
-    Simulator() = default;
-    int getVoertuigLastId()const;
-    void increaseVoertuigLastId();
-
-    [[nodiscard]] SimPrinter * getSimPrinter() const;
-
-    void setSimPrinter(SimPrinter * const sim_printer);
 
 
 
-    [[nodiscard]] vector<Voertuig *> getVoertuigen() const;
-
-    [[nodiscard]] vector<Verkeerslicht *> getVerkeerslichten() const;
-
-    [[nodiscard]] vector<Bushalte *> getBushaltes() const;
-
-    [[nodiscard]] vector<Voertuiggenerator *> getVoertuiggeneratoren() const;
 
 
-    void ToString();
-
-    double getSimulationTime() const;
-
-    double getincSimulationTime() const;
-
-    double incSimulationTime();
-
-    double UpdateSimulationTime() const;
 
 
-    void sortVoertuigenByPosition();
 
-    void sortVerkeersLichtByPosition();
 
-    void berekenPositie(Voertuig *v) const;
 
-    void BerekenVersnelling(Voertuig *v, int counter) const;
 
-    void BerekenSnelheidNaVertraging(Voertuig *v);
 
-    void BerekenSnelheidNaVersnelling(Voertuig *v);
-
-    void updateVoertuig(Voertuig *V, int counter) const;
-
-    bool isVoertuigOpBaan(const Voertuig *v);
-
-    void updateVoertuigAanVerkeerslichtSituatie(Verkeerslicht *l, int VerkeerslichtCounter);
-
-    bool isVoertuigInVertraagZone(Voertuig *v, Verkeerslicht *l);
-
-    bool isVoertuigInStopZone(Voertuig *v, Verkeerslicht *l);
-
-    vector<Voertuig *> voertuigenTussenVerkeerslichten(Verkeerslicht *lichtVoor, Verkeerslicht *lichtAchter);
-
-    vector<Verkeerslicht *> verkeerslichtenOpBaan(Verkeerslicht *licht);
-
-    void addVoertuig(Voertuig *v);
-    void addVerkeerslicht(Verkeerslicht *v);
-    void addBushalte(Bushalte *b);
-    void addVoertuiggenerator(Voertuiggenerator *v);
-
-    //dit maakt de simulatie string
-    void generateSimulation();
-
-    //dit zal de simulatie string na elk tijd updaten
-    void updateSimulation();
-
-    void voertuigenGenereren();
 
 
 };
