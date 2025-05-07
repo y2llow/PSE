4a4612e77fd3ca1014dbef7a9408a0dd789f7afb
y2llow <tst@gmail.com>
2025-05-07 15:54:11 +0200

    bug fixes

--- src/elementen/Constants.h
+++ src/elementen/Constants.h
@@ -5,7 +5,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-inline constexpr double SIMULATIE_TIJD = 0.166;
+inline constexpr double SIMULATIE_TIJD = 0.0166;
 inline constexpr double VERTRAAG_AFSTAND = 50;
 inline constexpr double STOP_AFSTAND = 15;
 inline constexpr double VERTRAAG_FACTOR = 0.4;
