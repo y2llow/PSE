34794bf85add307201c85215fbf3142b93292531
y2llow <tst@gmail.com>
2025-05-07 15:34:57 +0200

    bug fixes

--- main.cpp
+++ main.cpp
@@ -9,33 +9,33 @@ using namespace std;
 
 int main()
 {
- //    // ====== 4.1 Gui ====
- //    cout << R"(                      ____  ____  _____    ____                        _  _
- //  _____ _____ _____  |  _ \/ ___|| ____|  / ___|_ __ ___   ___ _ __   | || |    _____ _____ _____
- // |_____|_____|_____| | |_) \___ \|  _|   | |  _| '__/ _ \ / _ \ '_ \  | || |_  |_____|_____|_____|
- // |_____|_____|_____| |  __/ ___) | |___  | |_| | | | (_) |  __/ |_) | |__   _| |_____|_____|_____|
- //                     |_|   |____/|_____|  \____|_|  \___/ \___| .__/     |_|
- //                                                              |_|                                 )" << endl;
- //
- //    cout << "Start the simulation           [S]" << endl;
- //    cout << "Show the names of the students [T]" << endl;
- //    cout << "Exit the simulator             [X]" << endl;
- //
- //    char c;
- //    do
- //    {
- //        cout << "Please enter a valid character: ";
- //        cin >> c;
- //    }
- //    while (c != 'X' && c != 'T' && c != 'S');
- //
- //    if (c == 'X')
- //        return 0;
- //    if (c == 'T')
- //    {
- //        cout << "Abdellah El Moussaoui" << endl << "Eray Ayyildiz" << endl << "Yaman Haj Ahmad" << endl;
- //        return 0;
- //    }
+    // ====== 4.1 Gui ====
+    cout << R"(                      ____  ____  _____    ____                        _  _
+  _____ _____ _____  |  _ \/ ___|| ____|  / ___|_ __ ___   ___ _ __   | || |    _____ _____ _____
+ |_____|_____|_____| | |_) \___ \|  _|   | |  _| '__/ _ \ / _ \ '_ \  | || |_  |_____|_____|_____|
+ |_____|_____|_____| |  __/ ___) | |___  | |_| | | | (_) |  __/ |_) | |__   _| |_____|_____|_____|
+                     |_|   |____/|_____|  \____|_|  \___/ \___| .__/     |_|
+                                                              |_|                                 )" << endl;
+
+    cout << "Start the simulation           [S]" << endl;
+    cout << "Show the names of the students [T]" << endl;
+    cout << "Exit the simulator             [X]" << endl;
+
+    char c;
+    do
+    {
+        cout << "Please enter a valid character: ";
+        cin >> c;
+    }
+    while (c != 'X' && c != 'T' && c != 'S');
+
+    if (c == 'X')
+        return 0;
+    if (c == 'T')
+    {
+        cout << "Abdellah El Moussaoui" << endl << "Eray Ayyildiz" << endl << "Yaman Haj Ahmad" << endl;
+        return 0;
+    }
 
     // cout << "Pause the simulation   [P]" << endl;
     // cout << "Resume the simulation  [R]" << endl;
@@ -49,20 +49,20 @@ int main()
     }
 
 
-    // for (const auto b : sim->getBanen())
-    // {
-    //     b->sortVoertuigenByPosition();
-    //
-    //     sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width, 200), b->getNaam(),
-    //                             sf::Style::Close);
-    //
-    //     window.setFramerateLimit(60);
-    //     Game game = Game(&window);
-    //     game.loadMap("../output/baan_" + b->getNaam() + ".txt");
-    //
-    //
-    //
-    // }
+    for (const auto b : sim->getBanen())
+    {
+        b->sortVoertuigenByPosition();
+
+        sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width, 200), b->getNaam(),
+                                sf::Style::Close);
+
+        window.setFramerateLimit(60);
+        Game game = Game(&window);
+        game.loadMap("../output/baan_" + b->getNaam() + ".txt");
+
+
+
+    }
 
     return 0;
 }
